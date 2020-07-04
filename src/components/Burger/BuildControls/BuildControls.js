ce1031c1aa8e526753eba2ffe70bd9fb9b9837e6
Suyeon Kang <tndusrkd92@gmail.com>
2020-07-04 15:40:34 +0900

    Connecing state to BuildControls

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -13,7 +13,10 @@ const buildControls = props => (
     <div className={classes.BuildControls}>
         {
             controls.map(ctrl => (
-                <BuildControl key={ctrl.label} label={ctrl.label} />
+                <BuildControl 
+                key={ctrl.label} 
+                label={ctrl.label}
+                added={() => props.ingredientAdded(ctrl.type)} />
             ))}
     </div>
 );
--- src/container/BurgerBuilder/BurgerBuilder.js
+++ src/container/BurgerBuilder/BurgerBuilder.js
@@ -3,6 +3,13 @@ import React, { Component } from 'react';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
+const INGREDIENT_PRICES = {
+    salad: 0.5,
+    cheese: 0.4,
+    meat: 1.3,
+    bacon: 0.7
+};
+
 class BurgerBuilder extends Component {
     state = {
         ingredients: {
@@ -10,14 +17,36 @@ class BurgerBuilder extends Component {
             cheese: 0,
             meat: 0,
             bacon: 0
-        }
-    }
+        },
+        totalPrice: 4
+    };
+
+    addIngredientHandler = type => {
+
+        // Update count 
+        const oldCount = this.state.ingredients[type];
+        const updatedCount = oldCount + 1;
+        const updatedIngredients = { ...this.state.ingredients };
+        updatedIngredients[type] = updatedCount;
+
+        // Update price 
+        const priceAddition = INGREDIENT_PRICES[type];
+        const oldPrice = this.state.totalPrice;
+        const newPrice = oldPrice + priceAddition;
+
+        // Assign updated count and price to state
+        this.setState({ ingredients: updatedIngredients, totalPrice: newPrice });
+    };
+
+    removeIngredientHandler = type => {
+
+    };
 
     render() {
         return (
             <>
                 <Burger ingredients={this.state.ingredients} />
-                <BuildControls />
+                <BuildControls ingredientAdded={this.addIngredientHandler} />
             </>
         );
     }
