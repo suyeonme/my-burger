e00d41b27ae8b274d972df9e428c734d918aa6a7
Suyeon Kang <tndusrkd92@gmail.com>
2020-07-04 15:21:37 +0900

    Style and render BuildControlson UI

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import classes from './BuildControls.module.css';
+import BuildControl from './BuildControl/BuildControl';
+
+const controls = [
+    { label: 'Salad', type: 'salad' },
+    { label: 'Bacon', type: 'bacon' },
+    { label: 'Meat', type: 'meat' },
+    { label: 'Cheese', type: 'cheese' }
+];
 
 const buildControls = props => (
-    <div>
-        
+    <div className={classes.BuildControls}>
+        {
+            controls.map(ctrl => (
+                <BuildControl key={ctrl.label} label={ctrl.label} />
+            ))}
     </div>
 );
 
-export default BuildControls;
+export default buildControls;
