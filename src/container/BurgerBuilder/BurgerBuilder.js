5e95acb3a6a78f146b132cdcff61959011e04eed
Suyeon Kang <tndusrkd92@gmail.com>
2020-07-04 12:35:10 +0900

    Style burger and render it on UI
    Apply media query

--- src/container/BurgerBuilder/BurgerBuilder.js
+++ src/container/BurgerBuilder/BurgerBuilder.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Burger from '../../components/Burger/Burger'
 
 class BurgerBuilder extends Component {
     render() {
         return (
             <>
-                <div>Burger Builder</div>
+                <Burger />
                 <div>Build Controls</div>
             </>
         );
